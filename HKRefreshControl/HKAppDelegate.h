6c53145c4ecf376fd42efc34d63be43e868f62d6
Panos Baroudjian <baroudjian.panos@gmail.com>
2013-04-30 03:03:30 +0200

    Initial Commit

---
+++ HKRefreshControl/HKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HKAppDelegate.h
+//  HKRefreshControl
+//
+//  Created by Panos Baroudjian on 4/30/13.
+//  Copyright (c) 2013 Panos Baroudjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
